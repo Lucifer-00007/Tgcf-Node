3aeb54eb6d23d13850be76f4daed259607533ce7
[dyad] <files.backup.777@gmail.com>
2025-08-06 03:38:47 +0530

    [dyad] Updated sidebar icons - wrote 1 file(s)

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,63 +1,22 @@
-import React from 'react';
+import {
+  LayoutDashboard,
+  User,
+  KeyRound,
+  ShieldCheck,
+  Link,
+  Plug,
+  PlayCircle,
+  SlidersHorizontal,
+} from 'lucide-react';
 import { Page } from './types';
 
-const HandWaveIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-);
-
-const UserIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-  </svg>
-);
-
-const KeyIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
-  </svg>
-);
-
-const ShieldCheckIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3.25l6 2.25v5.25c0 4.97-3.2 8.47-6 9.75-2.8-1.28-6-4.78-6-9.75V5.5l6-2.25z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M9.5 11.75l1.75 1.75 3.25-3.25" />
-  </svg>
-);
-
-const LinkIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-  </svg>
-);
-
-const PlugIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M9 13l6-6m-1.5 7.5l3-3a3 3 0 10-4.243-4.243l-3 3M7.5 15L6 16.5a4.243 4.243 0 006 6L13.5 21" />
-  </svg>
-);
-
-const PlayCircleIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-    <circle cx="12" cy="12" r="9" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M10 8.5l5 3.5-5 3.5V8.5z" />
-  </svg>
-);
-
-const SlidersIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h10m-6 0v12M4 18h16m6-6H8m10 0V6" />
-  </svg>
-);
-
 export const NAV_ITEMS = [
-  { name: Page.Dashboard, icon: HandWaveIcon },
-  { name: Page.Profile, icon: UserIcon },
-  { name: Page.Credentials, icon: KeyIcon },
-  { name: Page.Admins, icon: ShieldCheckIcon },
-  { name: Page.Connections, icon: LinkIcon },
-  { name: Page.Plugins, icon: PlugIcon },
-  { name: Page.Run, icon: PlayCircleIcon },
-  { name: Page.Advanced, icon: SlidersIcon },
+  { name: Page.Dashboard, icon: LayoutDashboard },
+  { name: Page.Profile, icon: User },
+  { name: Page.Credentials, icon: KeyRound },
+  { name: Page.Admins, icon: ShieldCheck },
+  { name: Page.Connections, icon: Link },
+  { name: Page.Plugins, icon: Plug },
+  { name: Page.Run, icon: PlayCircle },
+  { name: Page.Advanced, icon: SlidersHorizontal },
 ];
